5aa5b0c1578a2461e6005113f9d6f0161b50b105
lappy <lappy@DESKTOP-DK7CVG9>
2022-11-25 16:08:55 +0530

    changedbygeter

--- try1/Startup.cs
+++ try1/Startup.cs
@@ -32,7 +32,7 @@ namespace try1
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World!ddd");
                 });
             });
         }
